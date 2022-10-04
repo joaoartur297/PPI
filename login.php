f9deda86fb7de89ae87e8158238da17eb08ac8d7
joaoartur297 <joao.2020302933@aluno.iffar.edu.br>
2022-10-04 00:22:16 -0300

    inicial

---
+++ login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="css/style.css"/>
+
+</head>
+<meta charset="utf-8"> 
+
+<title>Login</title>    
+ 
+
+<body>
+
+<h4>Bem vindo!</h4>
+
+<!-- painel login -->
+<div>
+
+<h1>Endereço de email</h1>
+<input type="text" placeholder="you@youremail.com">
+<h1>Senha</h1>
+<input type="password" placeholder="Senha">
+<br>
+<br>
+<button><h2>Entrar</h2></button> <h3>Esqueçeu sua senha?</h3>
+</div>
+
+<!-- botões -->
+
+<button class="cadastrar"><h2>Cadastrar</h2></button>
+<button class="entrar"><h2>Entrar</h2></button> 
+
+
+<?php
+
+
+?>
+
+
+</body>
+ 
+<footer>
+<img src="imagens/ondas.PNG"  width="100%" height="5%">
+</footer>
+ 
+</html>
